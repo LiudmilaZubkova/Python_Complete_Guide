606c0cc18f7731646038b10c518a1b5f5fbcf6c0
Liudmila Zubkova <lumilli@gmail.com>
2024-03-19 22:29:35 +0100

    types and conditions

---
+++ variables.py
@@ -0,0 +1,13 @@
+name = "Lucy"
+age = 35
+
+print(f"{name} is {age} years old")
+
+age = 36
+print(f"{name} is {age} years old")
+
+profession = 'software developer'
+print(f"{name} is {profession}")
+
+Name = 'Bob'
+print(f"{name} is not {Name}")
